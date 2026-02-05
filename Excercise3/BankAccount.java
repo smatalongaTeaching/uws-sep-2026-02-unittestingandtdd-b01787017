d9d4fbef904a20ce90ac81208173740fce784029
Dean Cain <b01787017@studentmail.uws.ac.uk>
2026-02-05 10:42:37 +0000

    Exercise 3: Implement Initial Account Balance - test passed

---
+++ Excercise3/BankAccount.java
@@ -0,0 +1,11 @@
+package Excercise3;
+
+public class BankAccount {
+
+    private double balance = 0.0;
+
+    public double getBalance() {
+        return balance; 
+    }
+
+}
--- Excercise3/BankAccountTest.java
+++ Excercise3/BankAccountTest.java
@@ -9,8 +9,5 @@ public class BankAccountTest {
         BankAccount account = new BankAccount();
         assertEquals(0.0, account.getBalance(), 0.001);
     }
-    
-    // Next test: testDepositIncreasesBalance
-    // Write this test, then implement the code to make it pass
-    // Continue with remaining features
+
 }
